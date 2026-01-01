32a894f92cde24846d76c680449458e96568faa6
刘岗强 <MrLiuGangQiang@users.noreply.github.com>
2026-01-01 12:22:14 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,15 +2,9 @@ import configData from './config.json' assert { type: 'json' };
 
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-
-    if (url.pathname === "/api/json") {
-      return new Response(JSON.stringify(configData), {
-        status: 200,
-        headers: { "Content-Type": "application/json; charset=utf-8" }
-      });
-    }
-
-    return new Response("Not Found", { status: 404 });
+    return new Response(JSON.stringify(configData), {
+      status: 200,
+      headers: { "Content-Type": "application/json; charset=utf-8" }
+    });
   }
 };
